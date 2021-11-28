690a0cb2720eebc4acc0a6146faa4f6939bd2008
Darkstick <franciskroft@hotmail.com>
2021-11-28 13:28:27 -0500

    movement added

--- Scripts/Creatures/player.gd
+++ Scripts/Creatures/player.gd
@@ -1,16 +1,30 @@
-extends Node
+extends KinematicBody2D
 
+export var maxspeed = 300
+export var jumpspeed = -1000
+export var gravity = 2000
+export (float, 0, 1.0) var friction = 0.2
+export (float, 0, 1.0) var acceleration = 0.25
+var velocity = Vector2.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func get_input():
+	var dir = 0
+	if Input.is_action_pressed("right"):
+		dir += 1
+	if Input.is_action_pressed("left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * maxspeed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+	if Input.is_action_pressed("down"):
+		pass
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if is_on_floor():
+		get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("up"):
+		if is_on_floor():
+			velocity.y = jumpspeed
